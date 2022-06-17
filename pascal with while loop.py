b3be295dab7fdd9c1a64c59f8b6fef717bdb8977
Mane Mkrtchyan <maneh.s.mkrtchyan@gmail.com>
2022-06-16 20:26:19 -0400

    pascal

---
+++ pascal with while loop.py
@@ -0,0 +1,13 @@
+from math import factorial
+def pascal(m, n):
+    res=0
+    i=0
+    while i<m:
+        j=0
+        while j<=i:
+            if i==m-1 and j==n-1:
+                res = factorial(i)//(factorial(j)*factorial(i-j))
+            j+=1
+        i+=1
+    return res
+print (pascal(7, 4))
