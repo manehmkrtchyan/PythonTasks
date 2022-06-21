5d7c7e2b8a0cab306d8b77b6991969dfea73fc30
Mane Mkrtchyan <maneh.s.mkrtchyan@gmail.com>
2022-06-21 06:43:18 -0400

    Added tasks to file

---
+++ ex.1.10.1.py
@@ -0,0 +1,81 @@
+"1"
+def info(name, adress):
+    print(f"my name is {name} and my adress is {adress}")
+
+name = input("input your name ")
+adress = input("input your adress ")
+info(name, adress)
+
+"2"
+name = input("Enter your name here ")
+def hello(name):
+    print(f"Hello dear {name}!")
+hello(name)
+ 
+"3"
+def area(a, b):
+    return f"res {a*b}"
+
+num1 = float(input("enter a "))
+num2 = float(input("enter b "))
+
+print(area(num1, num2))
+
+"4"
+def area_hoxamas(a, b):
+    return f"area is {a**b/43560}"
+
+num1 = float(input("enter a "))
+num2 = float(input("enter b "))
+
+print(area_hoxamas(num1, num2))
+
+"5"
+
+def price(m, n):
+    return f"{m*0.10+n*0.25} $"
+
+m = float(input("enter the quantity of low quality bottles "))
+n = float(input("enter the quantity of high quality bottles "))
+print(price(m, n))
+
+"6"
+price:float = float(input("enter the price of the order "))
+def teyavchar():
+    teyavchar = price*18/100
+    return f"{float(teyavchar):.2f}"
+def harker():
+    harker:float = price*20/100
+    return harker
+print(f"The price is {price} , the teyavchar is {teyavchar()}, the hark is {harker()}!")
+
+"7"
+def sum_from_1_to_tiv(tiv):
+    sum = 0
+    i = 1
+    while 1 < tiv:
+        sum += tiv
+        tiv -= 1
+    return sum
+tiv = int(input("enter the number"))
+print(sum_from_1_to_tiv(tiv))
+
+"8"
+quantity1 = int(input("Nermuceq manruqneri qanaky: "))
+quantity2 = int(input("Nermuceq hushanverneri qanaky: "))
+def yndhanur_qash(quantity1, quantity2):
+    return 75*quantity1 + 112*quantity2
+
+print(f"Dzer gnumneri yndhanur qashy kazmum e {yndhanur_qash(quantity1, quantity1)} gram.")
+
+"9"
+gumar = int(input("Enter the intitial summ of your money: "))
+def tari_1_2_3(gumar):
+    i = 0
+    while i < 3:
+        gumar = gumar + gumar*4/100
+        print (gumar)
+        i += 1
+tari_1_2_3(gumar)
+
+"10"
