c0367ac0a62708d868d4e0de4e07db23b1273695
Mane Mkrtchyan <maneh.s.mkrtchyan@gmail.com>
2022-06-24 07:21:24 -0400

    1-4

---
+++ gorcnakan3.py
@@ -0,0 +1,38 @@
+# "1"
+# tiv = int(input("enter the number: "))
+# a = 0
+# lst = []
+# while tiv != 0:
+#     a = tiv % 10
+#     lst.append(a)
+#     tiv = tiv // 10
+# num = [str(i) for i in lst]
+# nums = "".join(num)
+# print(int(nums))
+
+# "2"
+# tuple = (1, 2, 3)
+# num = "".join(str(i) for i in tuple)
+# print(num)
+
+# "3"
+# lst = [1, 5, 99, 3, -1]
+# max = lst[0]
+# min = lst[0]
+# for i in lst:
+#     if i > max:
+#         max = i
+#     elif i < min:
+#         min = i
+
+        
+# sum = max + min
+# print(sum)
+
+# "4"
+# lst = [1, 0, 5, 6, 8, 4, 9]
+
+# for i in range(0, len(lst)-1):
+#     if lst[i] % 2 == 0:
+#         print(i)
+    
