76ecffe8f7816efb37daf7262faac1b6dad2a75f
Mane Mkrtchyan <maneh.s.mkrtchyan@gmail.com>
2022-06-28 10:50:21 -0400

    1

---
+++ mysplit.py
@@ -0,0 +1,18 @@
+text = '127. 7. 45, 55'
+lst = []
+def mysplit(a):
+    ele = ''
+    for i in text:
+        if i != ",":
+            if i == a:
+                lst.append(ele)
+                ele = ''
+            else:
+                ele += i
+        else:
+            break
+    if ele != a:
+        lst.append(ele)
+    print(lst)
+mysplit(".")
+
