6f016f5cb608fc93942242ad07bf9f7b5fe3e215
Mane Mkrtchyan <maneh.s.mkrtchyan@gmail.com>
2022-06-28 17:12:28 -0400

    .

--- split_replace_join.py
+++ split_replace_join.py
@@ -1,30 +1,33 @@
-# source = input("enter the string: ")
-# sep = str(input("enter the separator: "))
-# count = int(input("enter the index, where you want to stop splitting: "))
-# lst = []
-# def split(source, sep, count):
-#     """This function returns the list of the words in the string usig sep as separator. 
-#     count parameter is the index of the maximum number of the splits to do.  """
-#     ele = ''
-#     for i in source:
-#         if source.index(i) != count:
-#             if i == sep:
-#                 lst.append(ele)
-#                 ele = ''
-#             else:
-#                 ele += i
-#         else:
-#             break
-#     if ele != sep:
-#         lst.append(ele)
-#     print(lst)
-# split(source, sep, count)
+source = input("enter the string: ")
+sep = str(input("enter the separator: "))
+index = int(input("enter the index where you want to stop splitting: "))
+lst = []
+def split(source, sep, index):
+    """This function returns the list of the words in the string usig sep as separator. 
+    index parameter is the index of the maximum number of the splits to do.  """
+    
+    ele = ''
+    for i in source:
+        if source.index(i) != index:
+            if i == sep:
+                lst.append(ele)
+                ele = ''
+            else:
+                ele += i
+        else:
+            break
+    if ele != sep:
+        lst.append(ele)
+    print(lst)
+    #ARMAN JAN ESTEX COUNTY CHSTACVEC ANEL, PAHANJY MIQICH POXEL EM, 
+    #COUNTI POXAREN INDEX OV EM AREL, VOR ED INDEXY UNECOX TARIC AJ SPLIT CHANI
+split(source, sep, index)
 
 
 source = input("enter the text: ")
 old = input("enter the substring you want to change: ")
 new = input("enter the new string: ")
-count = int(input("enter the index where you want to stop replacing: "))
+count = int(input("enter the number of the wods you want to change: "))
 def replace(source, old, new, count):
     result = ''
     lst = source.split(" ")
@@ -39,3 +42,18 @@ def replace(source, old, new, count):
 print(replace(source, old, new, count))
 
 
+
+
+data = []
+ele = input("Enter the words  ")
+while ele != '':
+    data.append(ele)
+    ele = input("Enter the words  ")
+sep = input("enter the separator")
+def join(data, sep):
+    res = ""
+    for i in data:
+        a = i + sep
+        res += a
+    return res
+print(join(data, sep))
