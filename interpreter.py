9cf34a8f9f5e3084e56bc1374b6e1821b8931b79
Mane Mkrtchyan <maneh.s.mkrtchyan@gmail.com>
2022-08-03 18:50:15 -0400

    homework

--- interpreter.py
+++ interpreter.py
@@ -20,7 +20,7 @@ for line in program_lines:
         if splitted[0] == 'PRINT':
             if (len(splitted) == 2) and (splitted[-1] == var):      
                 print(value)
-            elif len(splitted) > 2:    
+            elif len(splitted) >= 2:    
                 splitted.remove('PRINT')
                 for ele in splitted: 
                     print(ele, end = ' ')
@@ -32,27 +32,27 @@ for line in program_lines:
     the equals symbol('='), a spase(' ') and the value you want to assign to 
     the identifier. The value can be an expression, but you cannot use any spaces in it.
     """
+        
         if splitted[0] == 'VAR':
             var = splitted[1]
             value = splitted[-1]
             dict = {var: value}
 
+        
+        """
+    'IF' checks the condition given after the kewywoard (separated with a space). 
+    In case it returnes True value the set of code present after the condition will be executed.  
+    IF condition operation. 
+    """ 
+        def my_extend(self, other_list, index):
+            self[index:index] = other_list
+        if splitted[0] == 'IF':
+            cond = splitted[1]
+            if cond:
+                new_line = line[4 + len(cond)::]
+                my_extend(program_lines, list(new_line.split("\n")), program_lines.index(line) + 1)
+
+
     else:
         raise SyntaxError("The firs word of the line must be a keywoard.")   
     
-    
-     
-    # if splitted[0] == 'IF':
-    #     cond = splitted[1]
-    #     do = splitted[2::]
-    #     if cond:
-    #         program_lines.extend(index(line) + 1(do))
-
-    # elif 'IF' in splitted:
-    #     if splitted[1]:
-    #         if 'VAR' in splitted:
-    #             dict1 = {splitted[3]: splitted[-1]}
-    #         elif 'PRINT' in splitted:
-    #             print(splitted[-1])
-    #         else:
-    #             raise Exception("SyntaxError")
