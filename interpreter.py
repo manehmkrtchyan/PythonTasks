8f24e5bea6531edf416b2e147ecae55e68b73456
Mane Mkrtchyan <maneh.s.mkrtchyan@gmail.com>
2022-08-03 15:37:13 -0400

    homework

--- interpreter.py
+++ interpreter.py
@@ -1,4 +1,5 @@
 from logging import raiseExceptions
+from operator import index
 
 
 input_file = open('language.txt', 'r')
@@ -8,39 +9,45 @@ program_lines = input_text.split('\n') #Divides the text into lines and starts
                                        #interpreting the separate lines
 keywords = ["VAR", "PRINT", "IF"]
 
-"""
-'VAR' keyword declares a variable. Enter the keyword VAR, then the identifier (variable name) 
-then a spase(' '), the equals symbol('='), a spase(' ') and the value you want to assign to 
-the identifier. The value can be an expression, for example 5*5, but you cannot use any spaces in it.
-"""
-"""
-'PRINT' keywoard prints the expression that comes after the space following the keyword.
-For example 'PRINT Hello' will print 'Hello'.
-"""
-# def do_var():
-#     my_dict = {splitted[1]: splitted[-1]}
-# def do_print(word):
-#     print(word, end = ' ')    
-
 for line in program_lines:
     splitted = str(line).split(' ') 
     
-    if splitted[0] == 'PRINT':
-        splitted.remove('PRINT')
-        for ele in splitted: 
-            print(ele, end = ' ')
-        print()
-    
-    if splitted[0] == 'VAR':
-        my_dict =  {splitted[1]: splitted[-1]}
-        var = splitted[1]
+    if splitted[0] in keywords:
+        """
+    'PRINT' keywoard prints the expression that comes after the space following the keyword.
+    For example 'PRINT Hello world' will print 'Hello world'.
+    """ 
+        if splitted[0] == 'PRINT':
+            if (len(splitted) == 2) and (splitted[-1] == var):      
+                print(value)
+            elif len(splitted) > 2:    
+                splitted.remove('PRINT')
+                for ele in splitted: 
+                    print(ele, end = ' ')
+                print()
             
-    # if 'VAR' in splitted:
-    #     do_var()
-    #     var = splitted[1]
-    # if var in splitted:
-    #     print(var)
+        """
+    'VAR' keyword declares a variable. Enter the keyword VAR, 
+    then the identifier (variable name), then a spase(' '), 
+    the equals symbol('='), a spase(' ') and the value you want to assign to 
+    the identifier. The value can be an expression, but you cannot use any spaces in it.
+    """
+        if splitted[0] == 'VAR':
+            var = splitted[1]
+            value = splitted[-1]
+            dict = {var: value}
+
+    else:
+        raise SyntaxError("the firs word must be a keywoard.")   
     
+    
+    
+    # if splitted[0] == 'IF':
+    #     cond = splitted[1]
+    #     do = splitted[2::]
+    #     if cond:
+    #         program_lines.extend(index(line) + 1(do))
+
     # elif 'IF' in splitted:
     #     if splitted[1]:
     #         if 'VAR' in splitted:
